d56d05e6d255039a52ff5dae82d8dbf576b55584
rbtsoheib <taharsoheib08@gmail.com>
2026-01-16 20:24:28 +0100

    DAY 55 js

---
+++ js/DAY55.js
@@ -0,0 +1,18 @@
+
+const G = 345 ; 
+
+console.log(G) ;
+
+var R = "this is rabti soheib talking" ; 
+
+console.log(R) ;
+
+var S = "get the fuck out of my website" ;
+
+console.log(S) ;
+
+var T = "I was just joking hhhh, but seriously get out" ;
+
+console.log(T) ; 
+
+
