ff718bb3ba06096f9bef59280e7cb71d2bdeaa73
rbtsoheib <taharsoheib08@gmail.com>
2026-01-19 00:45:42 +0100

    DAY 57 js file

---
+++ js/DAY57.js
@@ -0,0 +1,19 @@
+
+
+var A = 2344 ;
+
+console.log(A) ;
+
+const C = "This is soheib talking" ; 
+const B = "It may seem to me like you are being a little rough are you not ?" ;
+var D = "I mean , let us take a quick look in the chances , i see 0% , seems pretty strange" ; 
+const G = "I suppose we all know where this is going right ? a GREAT place HHHHHHHHHHHH" ; 
+var E = "do not try please , as we mentioned earlier , it is 0% not close to it , but LITERALLY IT" ; 
+
+
+console.log(B);
+console.log(C);
+console.log(G);
+console.log(D);
+console.log(E);
+
