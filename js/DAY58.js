7b300ad851f2662a0171f08286295bdb044715f3
rbtsoheib <taharsoheib08@gmail.com>
2026-01-20 23:24:35 +0100

    modif

--- js/DAY58.js
+++ js/DAY58.js
@@ -1,20 +1,22 @@
-var A = "hello nigga ! hh "
+var A = "hello nigga ! hh ";
 
-console.log(A) ; 
+console.log(A);
 
+const C = "nigga in paris are going crazyyyyyyy !";
 
-const C = "nigga in paris are going crazyyyyyyy !"
+console.log(B);
 
-console.log(B) ;
+const D =
+  "well you cannot save a hoe from the streets buddy , the streets belongs to such girls";
 
-const D = "well you cannot save a hoe from the streets buddy , the streets belongs to such girls"
+console.log(C);
 
-console.log(C) ;
+const B =
+  "you say you want to buy her a drink , and she wants to go to Paris, and that is her form of love";
 
-const B = "you say you want to buy her a drink , and she wants to go to Paris, and that is her form of love"
+console.log(D);
 
-console.log(D) ;
+const E =
+  "her love language is theft and cheating , a total whore hhhhhhh !!!!";
 
-const E = "her love language is theft and cheating , a total whore hhhhhhh !!!!"
-
-console.log(E) ; 
+console.log(E);
