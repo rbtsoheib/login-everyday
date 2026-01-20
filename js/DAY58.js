7ed336eca7813bb8aefd854469f5aa648181c172
rbtsoheib <taharsoheib08@gmail.com>
2026-01-20 23:23:23 +0100

    day58 JS file

---
+++ js/DAY58.js
@@ -0,0 +1,20 @@
+var A = "hello nigga ! hh "
+
+console.log(A) ; 
+
+
+const C = "nigga in paris are going crazyyyyyyy !"
+
+console.log(B) ;
+
+const D = "well you cannot save a hoe from the streets buddy , the streets belongs to such girls"
+
+console.log(C) ;
+
+const B = "you say you want to buy her a drink , and she wants to go to Paris, and that is her form of love"
+
+console.log(D) ;
+
+const E = "her love language is theft and cheating , a total whore hhhhhhh !!!!"
+
+console.log(E) ; 
