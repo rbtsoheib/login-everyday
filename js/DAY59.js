87dc719276ab0bd9a55d7e8c7cd19764da9547ed
rbtsoheib <taharsoheib08@gmail.com>
2026-01-22 18:51:18 +0100

    day 59 js code

---
+++ js/DAY59.js
@@ -0,0 +1,14 @@
+var S =
+  "This is your captain speaking, you may want to hold onto your jackets because the fall will be HUGE";
+
+const R =
+  "the weather in the next 5 minutes is going to be as bad as possible, we expect some turbulence";
+
+const T =
+  "well let me tell you this you little cunt of a girl , I will not under any circumstance will allow this madness to happen again , you understand me ? now pack your shit and get the fuck out the damn castle";
+
+console.log(R);
+console.log(T);
+console.log(S);
+
+// all this monologue is happening inside my brain , if you are lucky enough to read it . FEEL FREE TO FUCK OFF CUNT ASS COCKSUCKER
