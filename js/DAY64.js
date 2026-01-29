d7abe580d1142c9bcb64ff754f69d0bc4a11316b
rbtsoheib <taharsoheib08@gmail.com>
2026-01-29 04:23:35 +0100

    day 64 js structure

--- js/DAY64.js
+++ js/DAY64.js
@@ -1,12 +1,12 @@
-
-const M = "I am listening to metallica - Enter Sandman while writing this , feels awsome ";
+const M =
+  "I am listening to metallica - Enter Sandman while writing this , feels awsome ";
 
 console.log(M);
 
-var m = M.length ;
+var m = M.length;
 
-console.log("The length of the string is : " + m ) ;
+console.log("The length of the string is : " + m);
 
-const N = M.toUpperCase() ;
+const N = M.toUpperCase();
 
-console.log(N) ;
+console.log(N);
