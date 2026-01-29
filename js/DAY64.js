252b04951af044c78fea6f4051af3031e394b9f3
rbtsoheib <taharsoheib08@gmail.com>
2026-01-29 04:22:03 +0100

    day 64 js

---
+++ js/DAY64.js
@@ -0,0 +1,12 @@
+
+const M = "I am listening to metallica - Enter Sandman while writing this , feels awsome ";
+
+console.log(M);
+
+var m = M.length ;
+
+console.log("The length of the string is : " + m ) ;
+
+const N = M.toUpperCase() ;
+
+console.log(N) ;
