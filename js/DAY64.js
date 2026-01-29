30133f6e77d3d40a4c0cd6793667e0921f229d8c
rbtsoheib <taharsoheib08@gmail.com>
2026-01-29 04:25:32 +0100

    day 64 js modif

--- js/DAY64.js
+++ js/DAY64.js
@@ -10,3 +10,24 @@ console.log("The length of the string is : " + m);
 const N = M.toUpperCase();
 
 console.log(N);
+
+const O =
+  " now I am listening to Ozzy Osbourne - no more tears, what a song man ";
+
+console.log(O);
+
+const P = N.concat(O);
+
+console.log(P);
+
+const Q = P.replace("OZZY", "Ozzy");
+
+console.log(Q);
+
+const R = Q.slice(0, 50);
+
+console.log(R);
+
+const S = Q.split(" ");
+
+console.log(S);
