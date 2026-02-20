a3bb648c5f48321624455abbd86518edbff95462
rbtsoheib <taharsoheib08@gmail.com>
2026-02-20 20:47:48 +0100

    day 72 js

---
+++ js/DAY72.js
@@ -0,0 +1,5 @@
+
+const Truth = "It was nevert meant for eren to wield the power of the founding titan, yet his father and the great faith made eren a vilain , sacreficing his mother to trick himself to hate the titans , his own people . ";
+
+console.log("this is the true story of a brave hero ." + Truth ) ;
+
