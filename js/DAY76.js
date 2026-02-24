0b72e6371b105b055e0c366c1730d424c5bfaa64
rbtsoheib <taharsoheib08@gmail.com>
2026-02-24 22:09:52 +0100

    day 76 js

---
+++ js/DAY76.js
@@ -0,0 +1,14 @@
+
+let b = "hbibiiiiiii" 
+
+if (b.length < 10) {
+
+    console.log("we do not accept short Hbibi's, GET BETTER")
+
+}
+
+else {
+
+    console.log("you do not have skill issue")
+
+}
