174912e426c3d213f6fe857e41d521c4b94db300
rbtsoheib <taharsoheib08@gmail.com>
2026-02-24 22:10:55 +0100

    modified the structure hhhhh, lets go

--- js/DAY76.js
+++ js/DAY76.js
@@ -1,14 +1,9 @@
-
-let b = "hbibiiiiiii" 
+let b = "hbibiiiiiii";
 
 if (b.length < 10) {
-
-    console.log("we do not accept short Hbibi's, GET BETTER, and you have skill issue")
-
-}
-
-else {
-
-    console.log("you do not have skill issue")
-
+  console.log(
+    "we do not accept short Hbibi's, GET BETTER, and you have skill issue",
+  );
+} else {
+  console.log("you do not have skill issue");
 }
